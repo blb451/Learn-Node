3cdaa962933be6cc45ff87b0c6015e484ae42201
Benjamin Brenner <ben.l.brenner@gmail.com>
2016-12-21 17:00:06 -0800

    3.11

---
+++ notes-node/app.js
@@ -0,0 +1,25 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
+
+// let filteredArray = _.uniq([1,2,3,3,2,1,2,3,4,2,1]);
+// console.log(filteredArray);
+
+// console.log(_.isString('string'));
+// console.log(_.isString(true));
+
+// console.log('Result:', notes.add(9, -2));
+
+// let res = notes.addNote();
+// console.log(res);
+
+// let user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err){
+//   if (err) {
+//     console.log('Unable to write file');
+//   }
+// });
