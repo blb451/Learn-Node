6a4daca244c7bd43b6cd0dd71f912dda6957f77e
Benjamin Brenner <ben.l.brenner@gmail.com>
2016-12-22 17:40:52 -0800

    3

---
+++ notes-node/playground/debugging.js
@@ -0,0 +1,9 @@
+var person = {
+  name: 'name'
+};
+
+person.age = 300;
+
+person.name = 'name2';
+
+console.log(person);
---
+++ notes-node/playground/json.js
@@ -0,0 +1,29 @@
+// var obj = {
+//   name: 'Name'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "Name, "age": 300}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(originalNoteString);
+
+console.log(typeof note);
+console.log(note.title);
