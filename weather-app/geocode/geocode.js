1b05dfad8fedc1c4cddbf1b70920186ff2408ce2
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-01-06 17:51:26 -0800

    4.33

---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+var geocodeAddress = (address) => {
+  var encodedAddress = encodeURIComponent(address);
+
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  }, (error, response, body)=> {
+    if (error) {
+      console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+      console.log('Unable to find that address.');
+      console.log (body.status)
+    } else if (body.status === 'OK') {
+      // console.log(JSON.stringify(response, undefined, 2));
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
+  });
+};
+
+
+
+module.exports.geocodeAddress = geocodeAddress;
