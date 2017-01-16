091099971c982da5274d8ea0ca305b57ab519b9f
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-01-15 17:59:48 -0800

    7.80

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log('Started on port 3000.')
+app.listen(port, () => {
+  console.log(`Started on port ${port}.`)
 });
 
 app.use(bodyParser.json());
@@ -30,6 +32,21 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 module.exports = {app};
